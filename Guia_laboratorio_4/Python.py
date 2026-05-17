73ee479b44861954302960e28ee1199c61a6c0a2
Mariela <N00477857@upn.pe>
2026-05-17 00:19:16 -0500

    Busqueda textual por ciudad

--- Guia_laboratorio_4/Python.py
+++ Guia_laboratorio_4/Python.py
@@ -31,7 +31,7 @@ usuarios = [
     {
         "nombre": "Jhordy",
         "email": "Jhordy24@gmail.com",
-        "Ciudad": "Cajamarca"
+        "ciudad": "Cajamarca"
     },
     {
         "nombre": "Luis",
@@ -49,3 +49,12 @@ usuarios = [
         "ciudad": "Piura"
     }
 ]
+
+# Búsqueda textual por ciudad
+buscar_ciudad = "Lima"
+
+print("\nUsuarios de la ciudad:", buscar_ciudad)
+
+for usuario in usuarios:
+    if usuario["ciudad"].lower() == buscar_ciudad.lower():
+        print(usuario["nombre"], "-", usuario["email"])
